236bfc78d84507942a079dfaac35d5beef645c9e
meghalakhwan <meghalakhwan9@gmail.com>
2023-08-09 15:10:59 +0530

    first commit

--- script.js
+++ script.js
@@ -54,15 +54,16 @@ function resetBoard(winner)
         }
     }
 
-    state.squares = Array(9).fill(null)
-    document.getElementById("player1Score").text(state.scores.player1)
-    document.getElementById("player2Score").text(state.scores.player2)
+    state.squares = Array(9).fill(null);
+    state.xIsNext = true;
+    $("#player1Score").text(state.scores.player1)
+    $("#player2Score").text(state.scores.player2)
     renderBoard();
 }
 
 function showWinner(winner)
 {
-    const alert_box = document.getElementById("alert-box")
+    const alert_box = $("#alert-box")
     if(winner)
     {
         if(winner === 'X')
@@ -87,9 +88,53 @@ function showWinner(winner)
 
 function renderSquare(index)
 {
-    const val = state.squares[index] ? state.squares[index] : "&nbsp;"
-    return `<div value = ${index} class = "box col-lg-4 col-md-4 col-sm-4 col-xs-4" onclick="boxClick(${index})">${val}</div>`
+    const val = state.squares[index] ? state.squares[index] : '&nbsp;';
+    return `<div value = ${index} class ="box col-lg-4 col-md-4 col-sm-4 col-xs-4" onclick="boxClick(${index})">${val}</div>`
 }
+
+function renderBoard()
+{
+    let board_html = ''
+    for(let i=0; i<9; i++)
+    {
+        board_html += renderSquare(i)
+    }
+    $("#board").html(board_html);
+    calculateWinner(state.squares)
+}
+
+
+
+function boxClick(index)
+{
+    const squares = state.squares;
+    if(calculateWinner(squares) )
+     {
+        return;
+     }
+
+     squares[index] = state.xIsNext ? 'X' : '0'
+     state.squares = squares;
+     state.xIsNext = !state.xIsNext;
+     renderBoard();
+
+}
+
+function resetGame(){
+    state.scores.player1 = 0
+    state.scores.player2 = 0
+    resetBoard(null);
+}
+
+
+
+$(() => {
+    renderBoard();
+    $("#alert-box").slideUp(0.0001);
+    $("#clear").on("click", ()=>resetBoard(null))
+    $("#reset").on("Click", () => resetGame());
+})
+
 
 
 
